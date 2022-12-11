6dc379bb0f8084ccac0c66d4d05a4266192d2eb0
anthonwellsjo <anthonwellsjo@gmail.com>
2022-12-11 16:34:37 +0100

    first commit

---
+++ hello_world.lua
@@ -0,0 +1,58 @@
+for x = 0,10,1
+do
+  name1 = "elliot"
+  name2 = "pappa"
+  print(name1.. "spelar minecraft "..x.." gånger")
+
+  -- print(name2.." pruttar "..x.." gånger")
+end
+
+
+
+for x =0, 10, 1
+do
+print(name2 .. " spelar med  "..name1..x.." gånger")
+print(name2 .. " bajsar med elliot "..name1..x.." gänger")
+end
+
+for x =0, 10, 1
+do
+print(name1.. " dödar i minecraft en zombie" ..name2.." tittar på mej" ..x.." gånger")
+end
+for x =0, 10, 1
+
+do
+
+end
+
+
+
+
+for i = 1, 10, 1 
+do
+print(name2 .."tittar på teve"..name1.."säger jag vill titta på teve")
+print(name1 .."tittar på teve"..name2.."säger jag vill titta på teve")
+print(name1 .."spelar minecraft"..name2.."säger jag vill spela minecraft")
+print(name1 .."spelar fut-ball"..name2.."säger kan jag spela fotboll med dej?")
+print(name2 .."tittar på skräck film"..name1.."säger kan jag titta på skräckfilm med dig?")
+print(name1 .."springer på gräset"..name2.."sager can jag springa med dej"..name1.."säger ja")
+for x =0, 10, 1
+do
+print(name2 .." bajsar medan "..name1.."spelar minecraft med otis ")
+print(name1 .." spelar minecraft med otis pappa "..name1.." tittar po teve ")
+print(name1 .." tittar på teve medan "..name2.."städar huset")
+end
+for i=0, 10, 1
+do  
+print(name1 .." tittar på teve. medan "..name1.."tittar po teve") 
+  )
+
+
+end
+
+
+  
+
+
+
+
